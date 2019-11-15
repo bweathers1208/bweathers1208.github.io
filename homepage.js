533cd01b12f2176741b5d1b0046950da7ff89711
Bri Weathers <bweathers1208@gmail.com>
2019-11-15 14:37:07 -0600

    responsive!

--- homepage.js
+++ homepage.js
@@ -1,21 +1,19 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
 
-//hide nav upon scroll down
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-40px";
-  }
-  prevScrollpos = currentScrollPos;
-} 
-
-//trying to show link text on scroll over image as menu
-function hover(element) {
-    element.setAttribute('my-image1', 'https://thepracticaldev.s3.amazonaws.com/i/33nr9dhmx07hgf5gcn31.jpg');
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
   }
-  
-  function unhover(element) {
-    element.setAttribute('my-image1', 'https://thepracticaldev.s3.amazonaws.com/i/gu3h6ie1iduu1olm61kc.jpg');
-  }
+}
