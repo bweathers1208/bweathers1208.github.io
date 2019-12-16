24edfbee80a3dba44f11483b4c5c27872403ce71
Bri Weathers <bweathers1208@gmail.com>
2019-12-16 14:19:05 -0600

    added alerts, etc to js file

--- BMI-Calculator.js
+++ BMI-Calculator.js
@@ -1,12 +1,19 @@
-//from Udemy class "The Complete 2020 Web Development Bootcamp," section 108
+var heightInput = prompt("What is your height?")
+var weightInput = prompt("What is your weight?")
 
+
+
+
+//from Udemy class "The Complete 2020 Web Development Bootcamp," section 108
 function bmiCalculator(weight, height) {
     var calc = weight / (height * height);
     return calc;
     }
    
-   var bmi = bmiCalculator(65, 1.8);
+var bmi = bmiCalculator(weightInput, heightInput);
    //result should be around 20
    
    console.log(bmi);
    //check what is logged in console to verify function is accurate
+
+alert("Your BMI is " + bmi + "!");
